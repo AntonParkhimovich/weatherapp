bc15e5e0cf470bfde04a824b551d8d121b9c5b26
Anton Parkhimovich <pad1907@gmail.com>
2021-06-09 22:37:00 +0300

    feat: add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+  },
+  globals: {
+    fetch: true,
+  },
+  extends: ["airbnb-base", "prettier"],
+  plugins: ["prettier"],
+  rules: {
+    
+  }
+};
