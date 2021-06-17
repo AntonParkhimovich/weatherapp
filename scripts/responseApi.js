8f60c41352528071054d0ee8b98f893fdf585527
Anton Parkhimovich <pad1907@gmail.com>
2021-06-17 20:54:57 +0300

    fix: fix acess key

--- scripts/responseApi.js
+++ scripts/responseApi.js
@@ -1,6 +1,6 @@
 function getResponse(responseCity) {
   return fetch(
-    `http://api.weatherstack.com/current?access_key=d13b7df89be670ac4a637e0c380afe75&query=${responseCity}`
+    `http://api.weatherstack.com/current?access_key=6ef04114106f0d26e10965a03e92a579&query=${responseCity}`
   )
     .then((response) => response.json())
     .then((response) => response);
