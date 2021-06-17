ff8b8f8dc5b1c1bfbed87633fefc24fb53aa6b1a
Anton Parkhimovich <pad1907@gmail.com>
2021-06-17 20:19:30 +0300

    init: add code project

---
+++ scripts/responseApi.js
@@ -0,0 +1,9 @@
+function getResponse(responseCity) {
+    return fetch(
+      `http://api.weatherstack.com/current?access_key=d13b7df89be670ac4a637e0c380afe75&query=${responseCity}`
+    )
+      .then((response) => response.json())
+      .then((response) => response);
+  }
+
+  module.exports = {getResponse};
